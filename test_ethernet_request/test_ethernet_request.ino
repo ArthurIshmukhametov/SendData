f0b88cce35264d5e314b9fb2e54078c78a576969
Arthur <aptyp1998@mail.ru>
2019-06-21 10:27:41 +0300

    Update test_ethernet_request.ino

--- test_ethernet_request/test_ethernet_request.ino
+++ test_ethernet_request/test_ethernet_request.ino
@@ -1,5 +1,4 @@
 #include <EtherCard.h>
-
 const char website[] PROGMEM = "jsonplaceholder.typicode.com";
 static byte session;
 Stash stash;
@@ -7,27 +6,21 @@ byte Ethernet::buffer[1000];
 static byte mymac[] = { 0xCC, 0x50, 0xE3, 0x8A, 0x9B, 0xDC};
 static byte myip[] = { 192, 168, 1, 8 };    //IPAddress(192, 168, 1, 8);
 static byte gwip[] = { 192, 168, 1, 1 };    //IPAddress(192, 168, 1, 1);
-
 static char * headers="\"Content-type\": \"application/json; charset=UTF-8\"";
 const char *  postval="{title: \"foo\",body: \"bar\", userId: 1}";
-
 void setup() {
   Serial.begin(115200);
   connectEthernet();
   sendEthernet();
  }
-int i = 0;
-
 void loop() {
   ether.packetLoop(ether.packetReceive());
- 
   const char* reply = ether.tcpReply(session);
   if (reply != 0) {
     Serial.println("Got a response!");
     Serial.println(reply);
   }
 }
-
 String sendEthernet(){
   Serial.println("[HTTP] Post...");
   byte sd = stash.create();
@@ -42,23 +35,16 @@ String sendEthernet(){
     "$H"),
   website, website, stash_size, sd);
   session = ether.tcpSend();
-  //ether.httpPost("https://jsonplaceholder.typicode.com", "/posts", headers, postval, callback);
 }
-
-
 void connectEthernet(){
-    
  if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 5)
     Serial.println(F("Failed to access Ethernet controller"));
   if (!ether.dhcpSetup())
-    Serial.println(F("DHCP failed"));
-
+  Serial.println(F("DHCP failed"));
   ether.printIp("IP:  ", ether.myip);
   ether.printIp("GW:  ", ether.gwip);
   ether.printIp("DNS: ", ether.dnsip);
-
-  if (!ether.dnsLookup(website))
-    Serial.println(F("DNS failed"));
-
+  if (!ether.dnsLookup(website)){
+    Serial.println(F("DNS failed"));}
   ether.printIp("SRV: ", ether.hisip);
 }
